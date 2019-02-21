6232d9ed753aea40b1cd031e22d6c52f9cc89b28
Ronson Shiigi <ronsonshiigi@Ronsons-MBP.home>
2019-02-20 21:35:28 -1000

    added git ignore

--- server.js
+++ server.js
@@ -3,15 +3,13 @@ const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
 
-let buzzWords = [];
+let buzzWords = {};
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  const { method } = req;
-
   res.send("hello yo mama");
 });
 
@@ -23,14 +21,21 @@ app
   .post((req, res) => {
     let buzz = req.body;
     let word = buzz.buzzWord;
+    let pointers = buzz.points;
+    let keys = Object.keys(buzzWords);
 
-    if (buzzWords.includes(word) === false) {
-      buzzWords.push(word);
+    if (keys.includes(word) === false) {
+      buzzWords[word] = pointers;
+      console.log(buzzWords);
       res.send({ success: true });
     } else {
       res.send({ success: false });
     }
     console.log(buzzWords);
+  })
+  .put((req, res) => {
+    console.log("PUTPUT Golf");
+    res.send("hi");
   });
 
 app.listen(PORT, () => {
